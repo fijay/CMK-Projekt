b7dd3878830531866ecb77cec698e54cc333ae7e
Finn Fahrenkrug <finnfahrenkrug@Finns-MacBook-Pro.local>
2017-07-20 12:31:57 +0200

    sound group

---
+++ Sound/Spaceship/Assets/Scripts/SpaceshipAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipAudio : MonoBehaviour {
+
+	public AudioClip sound;
+
+	void Awake (){
+		playAlienSound ();
+	}
+
+	public void playAlienSound(){
+		if (sound != null) {
+			SoundManager.instance.Loop(sound);
+		}
+	}
+}

ff0f257d6a749cf4d85cb14900b374e201ce7cb8
Deadkraut <A.stoldt1@gmx.de>
2017-07-20 11:51:14 +0200

    SocketClient
    I have add the SocketClient

---
+++ Server (Node.js, Socket.IO)/Client/SocketIO/SocketIO/ServerInfoJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SocketIO
+{
+    internal class ServerInfoJson
+    {
+        public string sound { get; set; }//Server
+        public string time { get; set; }
+        public string date { get; set; }
+        public DateTime dateTime { get; set; }
+        public string rotation { get; set; }//Server
+        public string size { get; set; } //Server
+    }
+}
